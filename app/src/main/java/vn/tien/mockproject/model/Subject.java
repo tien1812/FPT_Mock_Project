725c4e25cb86275b167e96551854ce0a89d4a993
tien1812 <vtien18121998@gmail.com>
2020-09-04 16:07:30 +0700

    create Home

---
+++ app/src/main/java/vn/tien/mockproject/model/Subject.java
@@ -0,0 +1,27 @@
+package vn.tien.mockproject.model;
+
+public class Subject {
+    private int mId;
+    private String mName;
+
+    public Subject(int id, String name) {
+        mId = id;
+        mName = name;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
